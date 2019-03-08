035f130b483a37bb3bd3b85ee64af20ecfac6892
Michael J. Pitz <michaeljpitz@gmail.com>
2019-03-08 15:42:08 +0100

    Update publicController.php

--- controller/publicController.php
+++ controller/publicController.php
@@ -8,14 +8,14 @@ if(isset($_GET['idnouvelle']) && ctype_digit($_GET['idnouvelle'])) {
 }elseif(isset($_GET['idcateg']) && ctype_digit($_GET['idcateg'])){
 
 $idcateg = (int)$_GET['idcateg'];
-require_once "view/detailNews.html.php";
+    require_once "view/rubNews.html.php";
 
 }elseif(isset($_GET['idauthor']) && ctype_digit($_GET['idauthor'])){
     $idauthor = (int)$_GET['idauthor'];
-    require_once "view/detailNews.html.php";
+    require_once "view/authorNews.html.php";
 
 }else{
-    header("Location: ./view.php");
+    require_once "view/accueilNews.html.php";
 }
 
 
