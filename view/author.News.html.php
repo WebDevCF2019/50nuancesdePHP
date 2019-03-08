abab5b216cde93c083278f65e998e984466f54e2
bogdanrusu1 <keloorap1@gmail.com>
2019-03-08 15:14:21 +0100

    author

--- view/author.News.html.php
+++ view/author.News.html.php
@@ -15,6 +15,10 @@ $sql_author= mysqli_query($mysqli,$mysqli_data) or die(mysqli_error($mysqli));
      </head>
 </body>
 
-<h1 class="author"></h1>
+<h1 class="author">Auteurs</h1>
+<p class="nom">Nom</p>
+<p class="bio">Bio</p>
+<p class="list">Liste de ses nouvelles </p>
+
 
 </html>
