fb44e8a2365403968e610fe7e5c1fddc70719c20
jedafinal <jedafinal@hotmail.com>
2019-03-08 09:56:55 +0100

    texte

---
+++ congif.php
@@ -0,0 +1,14 @@
+<?php
+
+define("DB_HOST", "127.0.0.1");
+define("DB_LOGIN", "root");
+define("DB_PWD", "");
+define("DB_NAME", "formulaire");
+define("DB_PORT", 3306);
+define("DB_CHARSET", "utf8");
+
+
+
+
+
+ 
