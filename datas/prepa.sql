cc1414edf9ba81024b6577021ae222a66c4bd8fd
jedafinal <jedafinal@hotmail.com>
2019-03-14 16:33:35 +0100

    basededonné

--- datas/prepa.sql
+++ datas/prepa.sql
@@ -3,3 +3,6 @@ SELECT * FROM article;
 SELECT * FROM categorie;
 SELECT * FROM permission;
 SELECT * FROM user;
+
+SELECT categorie.thetitle, article.thetitle, thedate, user.thename, left(thetext, 350) FROM article join categorie_has_article cha on article.idarticle= cha.article_idarticle join categorie on idcategorie= cha.categorie_idcategorie join user on user.iduser=article.user_iduser order by thedate desc;
+
