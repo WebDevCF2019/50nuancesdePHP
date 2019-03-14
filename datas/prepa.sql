b29bd88a87f9766e62205a0785fa5d012e7ef6c5
Silentt1 <lamjil_1997@yahoo.fr>
2019-03-14 15:37:21 +0100

    requete sql ajoutée

--- datas/prepa.sql
+++ datas/prepa.sql
@@ -3,3 +3,16 @@ SELECT * FROM article;
 SELECT * FROM categorie;
 SELECT * FROM permission;
 SELECT * FROM user;
+
+
+-- récupèrer la categorie en fonction de l'id
+
+select thetitle from categorie where idcategorie=1;
+
+-- récupère tous les articles de cette catégorie
+
+select DISTINCT article.thetitle from article,categorie,categorie_has_article 
+where 
+categorie_has_article.categorie_idcategorie = 1 
+and 
+article.idarticle=categorie_has_article.article_idarticle;
