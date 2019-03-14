b8b91e3817bd700f87b786630c74084f4728b64f
jeanbapt56 <toutafela@gmail.com>
2019-03-14 16:18:55 +0100

    text

--- datas/prepa.sql
+++ datas/prepa.sql
@@ -1,5 +1,7 @@
-# basic SELECT from nuancesdephp's tables
+
 SELECT * FROM article;
 SELECT * FROM categorie;
 SELECT * FROM permission;
 SELECT * FROM user;
+
+SELECT thetitle, thetext, thename, thedate, iduser, idarticle FROM article INNER JOIN user ON  user.iduser = article.idarticle;
