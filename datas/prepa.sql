6c88d8e06cad8a963a14cd5fe93bafa2867512bc
Stéphane <stephripa@gmail.com>
2019-03-14 16:21:33 +0100

    requeteAuteur

--- datas/prepa.sql
+++ datas/prepa.sql
@@ -2,4 +2,21 @@
 SELECT * FROM article;
 SELECT * FROM categorie;
 SELECT * FROM permission;
-SELECT * FROM user;
+SELECT * FROM user ;
+
+SELECT u.thename, u.thebio, a.thetitle, a.thetext, a.thedate
+FROM article a
+JOIN user u
+ON u.iduser = a.user_iduser
+where u.iduser = 1;
+
+
+
+
+SELECT a.thetitle, a.thetext, a.thedate, u.thename 
+FROM article a
+JOIN user u
+ON u.iduser=  a.user_iduser
+WHERE u.iduser = 1;
+
+
