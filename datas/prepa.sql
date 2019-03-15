740769c0b5321270b631413c191c74d04ac412dd
jedafinal <jedafinal@hotmail.com>
2019-03-15 13:31:51 +0100

    texte

--- datas/prepa.sql
+++ datas/prepa.sql
@@ -3,11 +3,9 @@ SELECT * FROM article;
 SELECT * FROM categorie;
 SELECT * FROM permission;
 SELECT * FROM user;
-
-<<<<<<< HEAD
 SELECT categorie.thetitle, article.thetitle, thedate, user.thename, left(thetext, 350) FROM article join categorie_has_article cha on article.idarticle= cha.article_idarticle join categorie on idcategorie= cha.categorie_idcategorie join user on user.iduser=article.user_iduser order by thedate desc;
 
-=======
+
 SELECT a.idarticle, a.thetitle, left(a.thetext,350) AS thetext, a.thedate,
 		u.iduser, u.thelogin, u.thename,
         c.idcategorie, c.thetitle as titlecateg
@@ -19,4 +17,4 @@ SELECT a.idarticle, a.thetitle, left(a.thetext,350) AS thetext, a.thedate,
 	INNER JOIN categorie c 
 		ON h.categorie_idcategorie = c.idcategorie
     ORDER BY a.thedate DESC;
->>>>>>> 148f7dbb2e621758f7346a1595a8bd6a6e643d64
+
