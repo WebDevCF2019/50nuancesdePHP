c5a2d77ecf225bd5e319bd2060e88f0c94e40b4c
jedafinal <jedafinal@hotmail.com>
2019-03-15 15:29:24 +0100

    texte

--- datas/prepa.sql
+++ datas/prepa.sql
@@ -3,18 +3,18 @@ SELECT * FROM article;
 SELECT * FROM categorie;
 SELECT * FROM permission;
 SELECT * FROM user;
-<<<<<<< HEAD
-SELECT categorie.thetitle, article.thetitle, thedate, user.thename, left(thetext, 350) FROM article join categorie_has_article cha on article.idarticle= cha.article_idarticle join categorie on idcategorie= cha.categorie_idcategorie join user on user.iduser=article.user_iduser order by thedate desc;
-=======
 
-<<<<<<< HEAD
+SELECT categorie.thetitle, article.thetitle, thedate, user.thename, left(thetext, 350) FROM article join categorie_has_article cha on article.idarticle= cha.article_idarticle join categorie on idcategorie= cha.categorie_idcategorie join user on user.iduser=article.user_iduser order by thedate desc;
+
+
+
 SELECT categorie.thetitle, article.thetitle, thedate, user.thename, left(thetext, 350) 
 FROM article 
 join categorie_has_article cha 
 on article.idarticle= cha.article_idarticle 
 join categorie on idcategorie= cha.categorie_idcategorie 
 join user on user.iduser=article.user_iduser order by thedate desc;
->>>>>>> 8d9c6bca53c23412d5a862a06444ee7105db555d
+
 
 
 # accueil - home page
@@ -27,19 +27,18 @@ SELECT a.idarticle, a.thetitle, left(a.thetext,350) AS thetext, a.thedate,
 		ON h.article_idarticle = a.idarticle
 	LEFT JOIN categorie c 
 		ON h.categorie_idcategorie = c.idcategorie
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     ORDER BY a.thedate DESC;
 
-=======
+
     GROUP BY a.idarticle    
     ORDER BY a.thedate DESC;
->>>>>>> 4677dedd76e3a12fc4e4095f70d89763e2cff306
-=======
+
+
     INNER JOIN user u
 		ON u.iduser = a.user_iduser    
     GROUP BY a.idarticle    
     ORDER BY a.thedate DESC;
     
     
->>>>>>> 4d9e79ab93c81bbdf8d5c0a08660f193f09c6434
+
