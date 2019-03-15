772d555dfa4c836880aba1aeb0bbd64f1e5b0d19
Silentt1 <lamjil_1997@yahoo.fr>
2019-03-15 15:07:59 +0100

    join et where finis

--- datas/prepa.sql
+++ datas/prepa.sql
@@ -11,6 +11,15 @@ select thetitle from categorie where idcategorie=1;
 
 -- récupère tous les articles de cette catégorie
 
+-- version join
+SELECT DISTINCT article.thetitle,article.thetext,article.thedate 
+FROM article
+JOIN categorie
+JOIN categorie_has_article 
+ON categorie_has_article.categorie_idcategorie = 3 
+AND article.idarticle=categorie_has_article.article_idarticle;
+
+-- version where
 select DISTINCT article.thetitle,article.thetext,article.thedate 
 from article,categorie,categorie_has_article 
 where categorie_has_article.categorie_idcategorie = 3 and article.idarticle=categorie_has_article.article_idarticle;
