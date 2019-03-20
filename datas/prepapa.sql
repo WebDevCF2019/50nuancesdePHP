c9e8076339efa8c2bce0d69d40f9dbacbaa3d64e
twisted355 <dimitribouvy@gmail.com>
2019-03-20 14:04:09 +0100

    correction supression de dossier local

---
+++ datas/prepapa.sql
@@ -0,0 +1,7 @@
+/* tables utilisateurs */
+SELECT * FROM article;
+SELECT * FROM categorie;
+SELECT * FROM permission;
+SELECT * FROM user;
+/* table de jointure many2many */
+SELECT * FROM categorie_has_article;
